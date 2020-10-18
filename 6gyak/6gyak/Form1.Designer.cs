77c9d8b5fb25b309f226a88d67ffd4d3fc5ab730
gunnergeri <zoldigergely2399@gmail.com>
2020-10-18 21:54:45 +0200

    otodikfeladat

--- 6gyak/6gyak/Form1.Designer.cs
+++ 6gyak/6gyak/Form1.Designer.cs
@@ -37,14 +37,14 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(24, 27);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(426, 387);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(638, 492);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
--- 6gyak/6gyak/Form1.cs
+++ 6gyak/6gyak/Form1.cs
@@ -7,20 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using _6gyak.Entities;
 
 namespace _6gyak
 {
     public partial class Form1 : Form
     {
-        BindingList<Entities.Ratedata> rates = new BindingList<Entities.Ratedata>();
+        BindingList<Ratedata> rates = new BindingList<Ratedata>();
 
+        private string result;
 
         public Form1()
         {
 
             InitializeComponent();
-            
-            dataGridView1.DataSource = rates;
+
+            Harmadik();
+            Negyedik();
+
+        }
+
+        private void Harmadik()
+        {
+           
 
             var mnbService = new MNBServiceReference.MNBArfolyamServiceSoapClient();
 
@@ -30,18 +40,42 @@ namespace _6gyak
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
             };
-
             var response = mnbService.GetExchangeRates(request);
 
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
 
-            
-            
+            dataGridView1.DataSource = rates;
+        }
 
+        private void Negyedik()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new Ratedata();
+                rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
 
 
 
-        
-    }
+}
 }
