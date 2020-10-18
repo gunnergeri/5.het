b70bbacf326215e5abfe1a4ce58f7385399dadce
gunnergeri <zoldigergely2399@gmail.com>
2020-10-18 21:04:39 +0200

    masodik

--- 6gyak/6gyak/Form1.cs
+++ 6gyak/6gyak/Form1.cs
@@ -12,9 +12,27 @@ namespace _6gyak
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
+            var mnbService = new MNBServiceReference.MNBArfolyamServiceSoapClient();
+
+            var request = new MNBServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+
             InitializeComponent();
         }
+
+        
     }
 }
