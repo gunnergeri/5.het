7d9e9dbca9cbbeb3ff3fcb9e30750b7e16965edb
gunnergeri <zoldigergely2399@gmail.com>
2020-10-25 22:21:07 +0100

    második

---
+++ week7/week7/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7.Entities
+{
+    public class BirthProbability
+    {
+        public double Kor { get; set; }
+        public double Gyerekszam { get; set; }
+        public double Vszinuseg { get; set; }
+    }
+}
---
+++ week7/week7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public double Kor { get; set; }
+        public double Halvszinuseg { get; set; }
+    }
+}
