be7bced3db3b4bd6f2a30b264d3ca16df06cc4ba
gunnergeri <zoldigergely2399@gmail.com>
2020-11-15 19:40:56 +0100

    cic

---
+++ week8/week8/Abstractions/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace week8.Abstractions
+{
+   public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        internal abstract void DrawImage(Graphics graphics);
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
---
+++ week8/week8/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week8.Abstractions;
+
+namespace week8.Entities
+{
+    public class Car : Toy
+    {
+        internal override void DrawImage(Graphics graphics)
+        {
+            ;
+        }
+    }
+}
